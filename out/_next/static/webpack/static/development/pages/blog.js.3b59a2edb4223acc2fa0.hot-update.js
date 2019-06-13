996d1410984b6f2517d79c89ef037a5c7498470f
Gareth <gareth@pitt-nash.com>
2019-06-13 22:13:15 +0100

    first npm run export performed

---
+++ out/_next/static/webpack/static/development/pages/blog.js.3b59a2edb4223acc2fa0.hot-update.js
@@ -0,0 +1,49 @@
+webpackHotUpdate("static/development/pages/blog.js",{
+
+/***/ "./pages/blog.js":
+/*!***********************!*\
+  !*** ./pages/blog.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Blog; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_layout_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/layout.js */ "./components/layout.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+var PostLink = function PostLink(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_2___default.a, {
+    as: "/p/".concat(props.id),
+    href: "/post?title=".concat(props.title)
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", null, props.title)));
+};
+
+function Blog() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_layout_js__WEBPACK_IMPORTED_MODULE_1__["default"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "paper"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("article", {
+    className: "paperHead"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "\xA0"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("article", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Coming Soon!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(PostLink, {
+    id: "hello-nextjs",
+    title: "Hello Next.js"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(PostLink, {
+    id: "learn-nextjs",
+    title: "Learn Next.js is awesome"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(PostLink, {
+    id: "deploy-nextjs",
+    title: "Deploy apps with Zeit"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Coming Soon!"))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=blog.js.3b59a2edb4223acc2fa0.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.eb80b5c82ef002815a0d.hot-update.js
@@ -0,0 +1,37 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_layout_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/layout.js */ "./components/layout.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/head */ "./node_modules/next-server/dist/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function Index() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_layout_js__WEBPACK_IMPORTED_MODULE_1__["default"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_2___default.a, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", null, "Gareth's place"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("meta", {
+    name: "viewport",
+    content: "initial-scale=1.0, width=device-width"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "paper"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("article", {
+    className: "paperHead"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "\xA0"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("article", {
+    contenteditable: "false"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Hello"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "I am guessing that you meant to come here, rather than it being a massive coincidence that you typed in a random URL and landed on this page, in which case, what are the chances of that?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("b", null, "Disclaimer"), ": that was a rhetorical question, although if you are so inclined to work it out, be my guest & tweet me your findings."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Welcome to the hopefully interesting, often nonsensical rantings of a software developer."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "I hope you enjoy your stay."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Gareth."))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.eb80b5c82ef002815a0d.hot-update.js.map
